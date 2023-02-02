230096373a15eb072ab1504bda492d6730fe526f
bobrenja <abcd43@ukr.net>
2023-02-02 12:29:36 +0200

    change state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,66 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Section from './Section/Section';
 import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './Feedback/FeedbackOptions';
 import css from './App.module.css';
 
-class App extends Component {
-  static defaultProps = {};
-  static propTypes = {};
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const buttonGroup = ['good', 'neutral', 'bad'];
+
+  const changeFeedback = name => {
+    if (name === 'good') setGood(() => good + 1);
+    if (name === 'neutral') setNeutral(() => neutral + 1);
+    if (name === 'bad') setBad(() => bad + 1);
   };
 
-  buttonGroup = ['good', 'neutral', 'bad'];
-
-  changeFeedback = name => {
-    this.setState(prevState => {
-      // console.log(prevState);
-      return {
-        [name]: prevState[name] + 1,
-      };
-    });
-  };
-
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce(
-      (acc, currentValue) => acc + currentValue,
-      0
-    );
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const res = (this.state.good / this.countTotalFeedback()) * 100;
+  const countTotalFeedback = () => good + neutral + bad;
+  const countPositiveFeedbackPercentage = () => {
+    const res = (good / countTotalFeedback()) * 100;
     return res ? Number(res.toFixed(1)) : 0;
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
+  return (
+    <div className={css.main}>
+      <Section title="Feedback caffe Expresso">
+        <FeedbackOptions
+          options={buttonGroup}
+          onLeaveFeedback={changeFeedback}
+        />
+      </Section>
 
-    return (
-      <div className={css.main}>
-        <Section title="Feedback caffe Expresso">
-          <FeedbackOptions
-            options={this.buttonGroup}
-            onLeaveFeedback={this.changeFeedback}
-          />
-        </Section>
-
-        <Section title="Statistics">
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={this.countTotalFeedback()}
-            positivePercentage={this.countPositiveFeedbackPercentage()}
-          />
-        </Section>
-      </div>
-    );
-  }
-}
-
-export { App };
+      <Section title="Statistics">
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        />
+      </Section>
+    </div>
+  );
+};
